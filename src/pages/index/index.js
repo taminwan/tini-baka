095c3071cab007a15a9e67ec765f8c57a2066458
taminwan <taminwan@gmail.com>
2022-07-07 13:50:41 +0700

    Add baka main tab

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,26 @@
 Page({
+  data: {
+    tabs: [
+      {
+        title: '⚡ Bid',
+      },
+      {
+        title: 'Hóng tin 👀',
+      },
+    ],
+    activeTab: 0,
+    isSwipeable: false,
+  },
+  handleTabClick({ index, tabsName }) {
+    this.setData({
+      [tabsName]: index,
+    });
+  },
+  handleTabChange({ index, tabsName }) {
+    this.setData({
+      [tabsName]: index,
+    });
+  },
   onLoad(query) {
   },
   onReady() {
