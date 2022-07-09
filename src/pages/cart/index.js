b9caba4cbac54dc93b603686a1f3c991e9c51098
taminwan <taminwan@gmail.com>
2022-07-09 16:13:41 +0700

    Add cart success confirm popup

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -8,5 +8,37 @@ Page({
 	onHide() {
 	},
 	onUnload() {
-	}
+	},
+
+  resetModal() {
+    this.setData({
+      modal: {},
+    });
+  },
+  onButtonClick(e) {
+    this.setData({
+      modal: {},
+    });
+  },
+  setModalState(nextModal) {
+    const newModalState = {
+      ...this.data.modal,
+      ...nextModal,
+    };
+
+    this.setData({
+      modal: newModalState,
+    });
+  },
+  changeImageSize(e) {
+    this.setModalState({
+      topImageSize: e.target.dataset.size,
+    });
+  },
+  onMaskClick() {
+    this.resetModal();
+  },
+  onTap(e) {
+    this.setModalState(e.target.dataset.modal);
+  },
 });
