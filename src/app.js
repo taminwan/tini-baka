bcacc5375cbd2559837bf076f40eb37ba42a6eb0
taminwan <taminwan@gmail.com>
2022-07-10 01:19:31 +0700

    Allow to get userInfo asap.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,22 @@ App({
     userAvatar: '',
     hasUser: false,
   },
+  getUserInfo() {
+    my.getUserInfo({
+      success: (res) => {
+        this.userAvatar = res.avatar;
+        this.userId = res.customerId;
+        this.userName = res.name;
+        this.hasUserData = true;
+      },
+      fail: (res) => {
+        console.log("fail", res);
+      },
+    });
+  },
   onLaunch(options) {
     console.log('App onLaunch');
+    this.getUserInfo();
   },
   onShow(options) {
   },
