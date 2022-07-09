f75951dc9e9347f883b9f611ee337c3541fd107a
sonbuiGFD <sonbui.gfd@gmail.com>
2022-07-10 01:05:48 +0700

    update count down component

--- src/components/count-down/index.js
+++ src/components/count-down/index.js
@@ -1,9 +1,60 @@
 Component({
-  data: {},
   props: {
     className: "",
-    deadline: "10/07/2022 12:00:00",
+    deadline: "Jul 11 2022 12:00:00 GMT+0700 (Indochina Time)",
   },
+  data: {
+    hours: 0,
+    minutes: 0,
+    secs: 0,
+  },
+  interval: null,
+  didMount() {
+    if (!this.props.deadline) {
+      return;
+    }
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    const self = this;
+    const deadline = new Date(this.props.deadline).getTime();
+    const now = new Date().getTime();
+    const countdownDuration = deadline - now;
+    const hours = Math.floor(
+      (countdownDuration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (countdownDuration % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const secs = Math.floor((countdownDuration % (1000 * 60)) / 1000);
+
+    this.setData({
+      hours,
+      minutes,
+      secs,
+    });
+
+    this.interval = setInterval(() => {
+      const now = new Date().getTime();
+      const countdownDuration = deadline - now;
+      const hours = Math.floor(
+        (countdownDuration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (countdownDuration % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const secs = Math.floor((countdownDuration % (1000 * 60)) / 1000);
+      self.setData({
+        hours,
+        minutes,
+        secs,
+      });
+    }, 1000);
+  },
+  didUnmount() {
+    clearInterval(this.interval);
+  },
+
   methods: {
     onClear() {
       this.props.onClear();
